814afd60ae869ec3419758f31f05ac858ef65a6c
Arora-505 <amiarora777@gmail.com>
2026-03-27 20:32:35 +0600

    Delete routes/alumni_Routes.js

--- routes/alumni_Routes.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const router=express.Router();
-const{get_all_alumni,add_alumni,get_alumni}=require("../controller/alumni_Controller");
-const validateToken=require("../middleware/validationTokenHandler");
-router.use(validateToken);
-
-router.route("/").get(get_all_alumni).post(add_alumni);
-router.route("/:id").get(get_alumni);
-module.exports=router;
