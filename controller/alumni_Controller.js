3ea34eced49cb736747a2b620ee612191e98edaf
Arora-505 <amiarora777@gmail.com>
2026-04-03 16:55:12 +0600

    Add files via upload

---
+++ controller/alumni_Controller.js
@@ -0,0 +1,77 @@
+const asyncHandler=require("express-async-handler");
+const alumni=require("../model/alumniModel");
+const get_all_alumni=asyncHandler(async (req, res) => {
+  const alumniData = await alumni.find({ user_id: req.user.id });
+  res.json(alumniData);
+});
+const get_alumni=asyncHandler(async(req,res)=>{
+    const alumniData = await alumni.findOne({
+        _id: req.params.id,
+        user_id: req.user.id
+    });
+
+    if(!alumniData){
+        res.status(404);
+        throw new Error("Alumni not found!");
+    }
+    res.status(200).json(alumniData);
+});
+
+const add_alumni=asyncHandler(async(req,res)=>{
+    const{name,
+        image,
+        profession,
+        roll,
+        email,
+        phone,
+        city,
+        currentLocation,
+        hometown,
+        company,
+        workSector,
+        university,
+        department,
+        batch,
+        school,
+        college,
+        bloodGroup,
+        parentsName,
+        interests,
+        bio,
+        linkedin,
+        github,
+        facebook,} =req.body;
+    if(!name || !email || !batch || !department || !profession || !company){
+        res.status(400);
+        throw new Error("all fields are mandatory!");
+    }
+    const alumniData=await alumni.create({
+    user_id:req.user.id,
+    name,
+        image,
+        profession,
+        roll,
+        email,
+        phone,
+        city,
+        currentLocation,
+        hometown,
+        company,
+        workSector,
+        university,
+        department,
+        batch,
+        school,
+        college,
+        bloodGroup,
+        parentsName,
+        interests,
+        bio,
+        linkedin,
+        github,
+        facebook});
+        
+    res.status(201).json(alumniData);
+});
+
+module.exports={get_alumni,add_alumni,get_all_alumni};
