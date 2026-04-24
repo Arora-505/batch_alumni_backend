82a2b0fe8da2f6f9a28b61f9c0fa5f0c2adf8295
Arora-505 <amiarora777@gmail.com>
2026-04-24 23:57:45 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -8,12 +8,21 @@ const path=require("path");
 db();
 const app=express();
 const staticPath = path.join(__dirname, "public");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"./views"));
 app.use(express.static(staticPath));
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", { title: "About Us" });
+});
 const port=process.env.PORT||5000;
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5500',
+    origin: 'http://localhost:5501',
     credentials: true
 }));
 app.use("/api/alumni",require("./routes/alumni_Routes"));
